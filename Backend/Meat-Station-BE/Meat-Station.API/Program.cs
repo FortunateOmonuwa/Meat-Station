9bbb0fcc6b4259da47e4cfb9ec8b9ea45280e007
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2023-12-16 15:28:28 +0100

    configure authentication

--- Backend/Meat-Station-BE/Meat-Station.API/Program.cs
+++ Backend/Meat-Station-BE/Meat-Station.API/Program.cs
@@ -5,8 +5,11 @@ using Meat_Station.DataAccess.Repositories;
 using Meat_Station.Domain.Models;
 using Meat_Station.Service.AuthService.Interfaces;
 using Meat_Station.Service.AuthService.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +23,32 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("AppConnection"));
+});
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.RequireAuthenticatedSignIn = true;
+})
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    var key = builder.Configuration["JWT:Key"];
+    if(key == null)
+    {
+        throw new ArgumentNullException(key);
+    }
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+    };
 });
 builder.Services.AddTransient<IAuthService, AuthService>();
 builder.Services.AddScoped<IRegisterService, RegisterService>();
@@ -41,18 +70,24 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "MeatStationAPI", Version = "v1" });
 });
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "MeatStationAPI v1");
+        
+    });
 }
 
-app.UseAuthentication();
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
